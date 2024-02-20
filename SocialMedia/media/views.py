c2aecbf31aaa28d90e50d6e5eeab3c1ea57244ce
Aiman <aimmharf05@gmail.com>
2024-02-20 16:50:46 -0500

    Started the register page

--- SocialMedia/media/views.py
+++ SocialMedia/media/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
-def hello():
-    print("Hello world")
+def index(request):
+    if not request.user.is_authenticated:
+        return render(request, "media/register.html")
+
+
+def register():
+    return
