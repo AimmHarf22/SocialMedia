2a35d26481e21e64a94501e638505405edda8d2b
Aiman <aimmharf05@gmail.com>
2024-02-21 18:26:31 -0500

    Implementing the register (backend)

--- SocialMedia/media/models.py
+++ SocialMedia/media/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class Users(AbstractUser):
+    # TODO: finish fixing the models
+    first_name = models.CharField()
+    last_name = models.CharField()
+    email = models.AbstractUser.email(blank=False)
+    username = models.AbstractUser.username(blank=False)
+    password = models.AbstractUser.password()
+    
--- SocialMedia/media/views.py
+++ SocialMedia/media/views.py
@@ -6,5 +6,37 @@ def index(request):
         return render(request, "media/register.html")
 
 
-def register():
-    return
+def register(request):
+    # Check For Request method
+    if request.method == 'POST':
+        # Post Data
+        first_name = request.POST['firstName']
+        last_name = request.POST['lastName']
+        email = request.POST['email']
+        username = request.POST['username']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        # VALIDATION
+        
+            # Check for empty boxes
+        if len(first_name) == 0 or len(last_name) == 0 or len(email) == 0 or len(username) == 0 or len(password) == 0 or len(password2) == 0:
+            return render(request, "register.html", {
+                'message': "All Fields Must Not Be blank"
+            })
+        
+            # Check if the passwords match
+        if password != password2:
+            return render(request, "register.html", {"message": "Password Does Not Match"})
+        
+            # Check if email is valid
+        if not '@' in email:
+            return render(request, "register.html", {"message": "Invalid Email"})
+        
+            # Check if password is strong
+        if not len(password) > 4:
+            return render(request, "register.html", {"message": "Password is weak"})
+        
+
+        # ADD DATA TO DATABASE
+        ...
